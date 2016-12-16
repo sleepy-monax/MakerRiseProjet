8d689e2c9716a3f5ad194a5665c0a053e4439f9c
makerdevteam <nicovb30@hotmail.com>
2016-12-16 15:41:30 +0100

    Update Control.cs

--- MakerRiseEngine.Core/UI/Controls/Control.cs
+++ MakerRiseEngine.Core/UI/Controls/Control.cs
@@ -11,64 +11,48 @@ namespace Maker.RiseEngine.Core.UI.Controls
         public Rectangle SizeBox;
         public Rectangle ClickRect;
 
-        //declare Envent Handeling
+        // Declare Envent Handeling
         public delegate void ClickEventHandler();
         public event ClickEventHandler OnMouseClick;
+        
+        // Mouse click sound effect.
         SoundEffectColection SE = SoundEffectParser.Parse("Engine", "ButtonClick");
 
+        // Mouse stats.
         public bool MouseOver = false;
         public bool MouseDown = false;
-
-
-
+        
         public virtual void Update(MouseState Mouse, KeyboardState KeyBoard, GameTime gameTime, int ContainerX, int ContainerY)
         {
+            // Update mouse clic hitbox.
             ClickRect = new Rectangle(SizeBox.X + ContainerX, SizeBox.Y + ContainerY, SizeBox.Width, SizeBox.Height);
+            
             // The active state from the last frame is now old
             lastMouseState = currentMouseState;
 
             // Get the mouse state relevant for this frame
             currentMouseState = Mouse;
-
+            
+            // Mouse is over the control?
+            MouseOver = ClickRect.Contains(Mouse.Position);
+            
             // Get if control containe the mouse pointer
-            if (ClickRect.Contains(Mouse.Position))
+            if (MouseOver)
             {
-
-                if (currentMouseState.LeftButton == ButtonState.Pressed)
-                {
-                    MouseDown = true;
-                }
-                else
-                {
-                    MouseDown = false;
-                }
-
-                MouseOver = true;
+                MouseDown = (currentMouseState.LeftButton == ButtonState.Pressed);
+                
                 // Recognize a single click of the left mouse button
                 if (lastMouseState.LeftButton == ButtonState.Pressed && currentMouseState.LeftButton == ButtonState.Released)
                 {
                     Audio.SoundEffectEngine.PlaySoundEffects(SE);
                     OnMouseClick?.Invoke();
                 }
-
             }
             else
-            {
-
                 MouseDown = false;
-                MouseOver = false;
-
-            }
-
-
         }
 
         public virtual void Draw(SpriteBatch spriteBatch, GameTime gameTime, int x, int y)
-        {
-
-            //doNothing();
-
-        }
-
+        {}
     }
 }
